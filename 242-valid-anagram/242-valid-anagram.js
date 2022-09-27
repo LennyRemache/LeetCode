00f085b69c806bd4459dd332171edccd6d5eedef
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-09-27 19:09:37 -0400

    Time: 180 ms (14.57%), Space: 48.4 MB (16.96%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,6 +4,8 @@
  * @return {boolean}
  */
 const isAnagram = (s, t) => {
+    if (s.length !== t.length) return false;
+    
     const sSorted = s.split('').sort().join('');
     const tSorted = t.split('').sort().join('');
     
