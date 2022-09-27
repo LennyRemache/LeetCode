f9781259f2ed05156814d982d1b0515399bfad21
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-09-26 21:42:47 -0400

    Time: 162 ms (15.54%), Space: 51.6 MB (77.64%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -7,11 +7,14 @@ const maxProfit = (prices) => {
     let maxProfit = 0;
     
     for(let i = 1; i < prices.length; i++) {
-        if(prices[i] < boughtPrice) {
+        if(prices[i] < boughtPrice) { // means there is a day where we can buy for cheaper
             boughtPrice = prices[i];
-        } else {
-            let profit = prices[i] - boughtPrice;
-            if (profit > maxProfit) {
+        } else { // curr day stock price is higher than bought price so there is potential
+                 // profit to be made. 
+            let profit = prices[i] - boughtPrice; // calculate profit thaat can be made
+            // this if condition's instructions only executes when the new profit calculation is 
+            // greater than a previous maxProfit calculation
+            if (profit > maxProfit) { // if profit is greater than maxProfit from calculation                                      // of other pairs of bought price and curr day price
                 maxProfit = profit;
             }
         }
