2ec3f4db566e0cb51ab33b5ae0c2e409da01561e
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-09-29 19:34:32 -0400

    Time: 115 ms (69.55%), Space: 42.9 MB (86.99%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -6,25 +6,24 @@
 const isAnagram = (s, t) => {
     if (s.length !== t.length) return false;
     
-    const sFreq = {};
-    const tFreq = {};
+    const letterFreq = {};
     
     for(let i = 0; i < s.length; i++) {
-        if (!sFreq[s[i]]) {
-            sFreq[s[i]] = 1;
+        if (!letterFreq[s[i]]) {
+            letterFreq[s[i]] = 1;
         } else {
-           sFreq[s[i]] += 1; 
-        }
-        
-        if (!tFreq[t[i]]) {
-            tFreq[t[i]] = 1;
-        } else {
-           tFreq[t[i]] += 1; 
+           letterFreq[s[i]] += 1; 
         }
     }
     
-    for (let char in sFreq) {
-        if (sFreq[char] !== tFreq[char]) return false
+    for(let j = 0; j < t.length; j++) {
+        if (letterFreq[t[j]]) {
+            letterFreq[t[j]] -= 1; 
+        }   
+    }
+    
+    for (let char in letterFreq) {
+        if (letterFreq[char] !== 0) return false;
     }
     
     return true;
