077c7db94da69b88ab8ceb0fea15423981865abd
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-01 15:27:46 -0400

    Time: 130 ms (29.19%), Space: 44.7 MB (24.68%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+const floodFill = (image, sr, sc, color) => { // color: number to replace with, image[sr][sc]: has the starting color
+    if (image[sr][sc] === color) {
+        return image;
+    }
+    const startColor = image[sr][sc];
+    
+    let fill = (image, sr, sc, startColor, color) => {
+        if (sr < 0 || sr > image.length - 1 || sc < 0 || sc > image[0].length - 1) {    // if index out of bounds
+            return;
+        } else if(image[sr][sc] === startColor) {
+            image[sr][sc] = color;
+            // up
+            fill(image, sr - 1, sc, startColor, color)
+            // down
+            fill(image, sr + 1, sc, startColor, color)
+            // left
+            fill(image, sr, sc - 1, startColor, color)
+            // right
+            fill(image, sr, sc + 1, startColor, color)
+        }
+            
+    }
+    
+    fill(image, sr, sc, startColor, color);
+    
+    return image;
+};
