d7444c5f01d43a4c8f7e8de07c56ed44ff321758
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-01 17:34:03 -0400

    Time: 125 ms (38.00%), Space: 44.5 MB (32.29%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -12,25 +12,27 @@ const floodFill = (image, sr, sc, color) => { // color: number to replace with, 
     const startColor = image[sr][sc]; // need to store what the starting row and column value's starting color was in order to make sure
                                       // all neighboring cells have the same starting color as the original starting cell
     
-    // helper function
-    let fill = (image, sr, sc, startColor) => {
+    // helper function needed to prevent overwriting startColor variable
+    let fill = (image, sr, sc) => {
         if (sr < 0 || sr > image.length - 1 || sc < 0 || sc > image[0].length - 1 || image[sr][sc] !== startColor) { // if index out of bounds
             return;                                                                                                  // or cell doesnt have startingColor   
         } 
         
-        // else we are safe to modify the current cell to the color passed into floodFill
+        // else we are safe to modify the current cell to the color passed into floodFill()
         image[sr][sc] = color;
+        
+        // then we can also make recursive calls on the cells above, below, left, right to the current cell
         // up
-        fill(image, sr - 1, sc, startColor)
+        fill(image, sr - 1, sc)
         // down
-        fill(image, sr + 1, sc, startColor)
+        fill(image, sr + 1, sc)
         // left
-        fill(image, sr, sc - 1, startColor)
+        fill(image, sr, sc - 1)
         // right
-        fill(image, sr, sc + 1, startColor)
+        fill(image, sr, sc + 1)
     }
     
-    fill(image, sr, sc, startColor);
+    fill(image, sr, sc); // first call to helper function that 
     
     return image;
 };
