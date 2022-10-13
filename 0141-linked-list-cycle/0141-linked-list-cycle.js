b25a3c0ea7201f1ef9239a1581df69d29b17960f
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-13 19:05:21 -0400

    Time: 125 ms (37.70%), Space: 45.2 MB (35.13%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = (head) => {
+    let fastPointer = head;
+    let slowPointer = head;
+    
+    while(fastPointer && fastPointer.next) {
+        fastPointer = fastPointer.next.next;
+        slowPointer = slowPointer.next;
+        
+        if (fastPointer === slowPointer) return true;
+    }
+    
+    return false;
+};
