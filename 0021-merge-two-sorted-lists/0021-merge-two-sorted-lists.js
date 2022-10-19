2c070d45f0b59baa9b025cfb070b45d86a18c821
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-19 18:31:24 -0400

    Time: 115 ms (44.67%), Space: 43.7 MB (94.83%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -15,21 +15,15 @@ const mergeTwoLists = (list1, list2) => {
     const head = res;
     
     while (list1 && list2) {
-        if (list1.val < list2.val) {
+        if (list1.val <= list2.val) {
             res.next = list1;
             list1 = list1.next
         }
-        else if (list2.val < list1.val) {
+        else {
             res.next = list2;
             list2 = list2.next;
         }
-        else {
-            res.next = list1;
-            list1 = list1.next;
-        }
-        
-        res = res.next;
-        
+        res = res.next;    
     }
     
     if (list1) {
