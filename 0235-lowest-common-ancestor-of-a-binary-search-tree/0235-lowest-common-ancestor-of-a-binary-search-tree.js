83951a6d7c9d397705d1508b034be39accf46a15
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-21 17:11:57 -0400

    Time: 125 ms (69.52%), Space: 51.7 MB (91.58%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+const lowestCommonAncestor = (root, p, q) => {
+    let common = root;
+    
+    while(root) {
+        if ((p.val < root.val && q.val > root.val) || (p.val > root.val && q.val < root.val)) return root;
+        if (p.val < root.val && q.val < root.val) {
+            root = root.left;
+        } else if (p.val > root.val && q.val > root.val) {
+            root = root.right;
+        } else return root;
+    }
+    
+    return common;
+};
