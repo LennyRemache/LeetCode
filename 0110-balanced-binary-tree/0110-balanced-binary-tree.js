0ce206a18350e0841f085760bf834b6a7b0f8f7c
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-23 14:30:03 -0400

    Time: 115 ms (64.93%), Space: 47.5 MB (39.27%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -10,21 +10,20 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
-    let res = true;
-    
-    let maxDepth = (node) => {
-        if (!node) return 0;
+const isBalanced = (root) => {
+    let balanced = true;
+    const getDepth = (node) => {
+        if (node === null) return 0;
         
-        let left = maxDepth(node.left);
-        let right = maxDepth(node.right);
+        const leftHeight = getDepth(node.left)
+        const rightHeight = getDepth(node.right)
         
-        if (Math.abs(left- right) > 1) res = false;
+        if (Math.abs(leftHeight - rightHeight) > 1) balanced = false;
         
-        return 1 + Math.max(left, right);
+        return Math.max(leftHeight, rightHeight) + 1
     }
     
-    maxDepth(root)
-   
-    return res;
+    getDepth(root);
+    
+    return balanced;
 };
