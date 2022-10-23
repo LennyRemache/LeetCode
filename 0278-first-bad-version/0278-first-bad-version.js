e8f66a9ddf15a6dfa79fbfd86c5ed75ac2b4febc
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-10-23 15:20:38 -0400

    Time: 91 ms (56.53%), Space: 42.1 MB (17.83%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -20,17 +20,14 @@ const solution = (isBadVersion) => {
     return function(n) {
         let start = 0;
         let end = n;
-        
-        while (start < end) {
-            let mid = Math.floor((start + end) / 2);
-            if (isBadVersion(mid)) {
-                end = mid;
-            } else {
+        while(start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            if (!isBadVersion(mid)) {
                 start = mid + 1;
+            } else if (isBadVersion(mid)){
+                if (!isBadVersion(mid - 1)) return mid;
+                else end = mid - 1;
             }
-            
         }
-        
-        return end;
     };
 };
