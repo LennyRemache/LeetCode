d21a683c5409b8cd8b6144f5f0ba73ff1236a975
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-11-05 18:51:37 -0400

    Time: 97 ms (92.87%), Space: 46.4 MB (81.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,13 +5,15 @@
 const lengthOfLongestSubstring = (s) => {
     let substring = new Map();
     
-    let start = 0;
+    let start = 0; // used to keep track of letter indices that were removed. set to 0 because none were removed yet
     let maxLength = 0;
     
     for (let i = 0; i < s.length; i++) { // loop through entire string once
         while (substring.has(s[i])) { // remove characters in the front of the Map 1 by 1 until the curr char is not in the Map
-            substring.delete(s[start])
-            start += 1; // this 
+            substring.delete(s[start]) // Map.delete() is O(1) time complexity
+            start += 1; // this allows us to remove letters to the left of curr char until the duplicate is removed.
+                        // 1 is added to keep track of where the start index of the current substring is since s is not modified directly
+                        // also because we cant delete based on indices for a Map otherwise I wouldve done substring.delete(0) multiple times;
         }
         substring.set(s[i]); // Map.has() is constant time complexity
         
