77faf7c18253e6e93e011e0af925cb1d5a174be9
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-11-05 18:45:42 -0400

    Time: 96 ms (93.19%), Space: 46.4 MB (81.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
-    let window = new Map();
+    let substring = new Map();
     
     let start = 0;
     let maxLength = 0;
     
     for (let i = 0; i < s.length; i++) { // loop through entire string once
-        while (window.has(s[i])) { // remove characters in the front of the Map 1 by 1 until the curr char is not in the Map
-            window.delete(s[start])
+        while (substring.has(s[i])) { // remove characters in the front of the Map 1 by 1 until the curr char is not in the Map
+            substring.delete(s[start])
             start += 1; // this 
         }
-        window.set(s[i]); // Map.has() is constant time complexity
+        substring.set(s[i]); // Map.has() is constant time complexity
         
-        maxLength = Math.max(maxLength, window.size);
+        maxLength = Math.max(maxLength, substring.size);
     }
     
     return maxLength;
