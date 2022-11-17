fa5c10d0838f512c136cf068f783614dec02d01c
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-11-16 19:15:17 -0500

    Time: 83 ms (96.50%), Space: 50.1 MB (86.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 const maxSubArray = (nums) => {
-    let sum = nums[0];
+    let maxSum = nums[0];
     for (let i = 1; i < nums.length; i++) {
-        nums[i] = Math.max(nums[i] + nums[i- 1], nums[i]);
-        sum = Math.max(nums[i], sum);
+        nums[i] = Math.max(nums[i], nums[i-1] + nums[i]);
+        maxSum = Math.max(maxSum, nums[i]);
     }
-    return sum;
+    
+    return maxSum;
 };
