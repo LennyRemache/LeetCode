ede1d2160030a4655b651c475ebf2cb2d75e8b88
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 18:28:12 -0500

    Time: 129 ms (77.99%), Space: 46.7 MB (76.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,24 +3,24 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
-    let substring = new Map();
+    let appeared = new Map();
+    let start = 0;
+    let end = 0;
     
-    let start = 0; // used to keep track of where our substring starts when removing chars from the Map
-                   // no chars removed so our substring starts at index 0. if a char is removed then our substring starts at index 1 etc.
     let maxLength = 0;
     
-    for (let i = 0; i < s.length; i++) { // loop through entire string once
-        while (substring.has(s[i])) { // remove characters in the front of the Map 1 by 1 until the curr char is not in the Map
-            substring.delete(s[start]) // Map.delete() is O(1) time complexity
-            start += 1; // this allows us to remove letters to the left of curr char until the duplicate is removed.
-                        // 1 is added to keep track of where the start index of the current substring is since s is not modified directly
-                        // also because we cant delete based on indices for a Map otherwise I wouldve done substring.delete(0) multiple times;
-        }
-        substring.set(s[i]); // Map.has() is constant time complexity
+    while (end < s.length) {
+        let endChar = s[end];
         
-        maxLength = Math.max(maxLength, substring.size);
+        while (appeared.has(endChar)) {
+            appeared.delete(s[start]);
+            start++;
+        }
+        
+        appeared.set(endChar, true);
+        maxLength = Math.max(maxLength, (end - start) + 1);
+        end++;
     }
     
     return maxLength;
-    
 };
