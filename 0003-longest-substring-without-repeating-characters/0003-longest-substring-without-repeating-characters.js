461353af5855184146632ea732a3f1e44da9a3de
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 18:35:49 -0500

    Time: 121 ms (82.73%), Space: 46.6 MB (78.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,23 +3,31 @@
  * @return {number}
  */
 const lengthOfLongestSubstring = (s) => {
+    // T: O(2n) -> O(n)  S: O(n)
     let appeared = new Map();
-    let start = 0;
-    let end = 0;
     
-    let maxLength = 0;
+    // substrings must be continous with no repeating characters
+    let start = 0; // indicates the start index of a substring
+    let end = 0; // indicate the end index of a substring
     
+    let maxLength = 0; // used to store the max length of each substring without repeating characters
+    
+    // while our end index of a possible substring does not go out of bounds
     while (end < s.length) {
         let endChar = s[end];
         
+        // if the current end char appeared previously in the current substring
+        // then we must remove the start of the substring until we no longer have a repeated char
         while (appeared.has(endChar)) {
             appeared.delete(s[start]);
             start++;
         }
+        // while loop will not run if the current ending char did not appear previously 
+        // so we can safely store it in the appeared Map()
         
         appeared.set(endChar, true);
-        maxLength = Math.max(maxLength, (end - start) + 1);
-        end++;
+        maxLength = Math.max(maxLength, (end - start) + 1); // check current valid substring's length against maxLength
+        end++; // increment the end index
     }
     
     return maxLength;
