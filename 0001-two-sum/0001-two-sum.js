6ffdc1067bf8834f063e85d7e94afc717c2456d6
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 19:14:50 -0500

    Time: 95 ms (84.91%), Space: 42.9 MB (35.40%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,10 +4,13 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-    const pair = {};
-    
+    let targets = {};
     for (let i = 0; i < nums.length; i++) {
-        if (pair[nums[i]] === undefined) pair[target-nums[i]] = i;
-        else return [pair[nums[i]], i];
+        let currNum = nums[i];
+        if (targets[currNum] === undefined) {
+            targets[target - currNum] = i;
+        } else {
+            return [i,targets[currNum]];
+        }
     }
 };
