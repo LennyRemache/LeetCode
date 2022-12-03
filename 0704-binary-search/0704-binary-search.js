669b8df6024644bdf4fa5e596193826e31990c51
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-03 16:33:40 -0500

    Time: 108 ms (43.24%), Space: 45.3 MB (20.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -8,11 +8,16 @@ const search = (nums, target) => {
     let end = nums.length - 1;
     
     while (start <= end) {
-        let mid = Math.floor((start + end) / 2);
+        const mid = Math.floor((start + end) / 2);
         
-        if (nums[mid] === target) return mid;
-        else if (nums[mid] < target) start = mid + 1;
-        else end = mid - 1;
+        if (nums[mid] === target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            end = mid - 1;
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        }
     }
     
     return -1;
