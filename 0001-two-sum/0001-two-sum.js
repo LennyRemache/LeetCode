3f243bf1d4ea1dbb28bb1b1599eb2c2e12437257
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 19:23:30 -0500

    Time: 67 ms (97.38%), Space: 43.1 MB (28.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,12 +5,19 @@
  */
 const twoSum = (nums, target) => {
     // T: O(n) S: O(n)
-    let targets = {};
+    let targets = {}; // used to store the pair value for each num in the nums arr that would sum to the 
+                      // target, along with the curr nums index as the value
+    
+    // loop through each num in the nums array until we find the solution
     for (let i = 0; i < nums.length; i++) {
-        let currNum = nums[i];
-        if (targets[currNum] === undefined) {
+        let currNum = nums[i]; // 
+        if (targets[currNum] === undefined) { // if the curr num is not a pair for a previously seen number that sums up to the target
+                                              // then we can instead store the number that correlates with curr num to sum up to pair along with
+                                              // curr nums index
+                                              // so -> target - currNum = the correlated number we are looking for 
             targets[target - currNum] = i;
-        } else {
+        } else { // else if the curr num is a pair for a previously seen number that sums up to the target
+                                              // then we can return the stored index for currNum's pair in hashmap, and the currNums index
             return [targets[currNum], i];
         }
     }
