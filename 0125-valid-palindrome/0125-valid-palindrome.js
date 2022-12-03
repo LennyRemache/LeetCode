c49585b53b4334561d216c149ce609e26109c02c
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 22:12:50 -0500

    Time: 68 ms (98.23%), Space: 43.9 MB (92.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,14 +4,31 @@
  */
 const isPalindrome = (s) => {
     s = s.toLowerCase();
-    let newS = ""
     
-    for (let i = 0; i < s.length; i++) {
-        const isLetter = s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122;
-        const isNum = s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57;
+    let left = 0;
+    let right = s.length - 1;
+    
+    while (left < right) {
+        let isLeftAlpha = s.charCodeAt(left) >= 97 && s.charCodeAt(left) <= 122;
+        let isLeftNum = s.charCodeAt(left) >= 48 && s.charCodeAt(left) <= 57;
         
-        if (isLetter || isNum) newS += s[i];
+        let isRightAlpha = s.charCodeAt(right) >= 97 && s.charCodeAt(right) <= 122;
+        let isRightNum = s.charCodeAt(right) >= 48 && s.charCodeAt(right) <= 57;
+        
+        if (!isLeftAlpha && !isLeftNum) {
+            left++;
+            continue;
+        }
+        if (!isRightAlpha && !isRightNum) {
+            right--;
+            continue;
+        }
+        
+        if (s[left] !== s[right]) return false;
+        
+        left++;
+        right--;
     }
     
-    return newS === newS.split('').reverse().join('');
+    return true;
 };
