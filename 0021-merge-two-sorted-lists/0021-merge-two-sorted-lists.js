dfb388f1b6c82c647184ad9edb6b1ed7230023fb
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 21:03:02 -0500

    Time: 75 ms (89.67%), Space: 44.1 MB (58.34%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -14,9 +14,10 @@ const mergeTwoLists = (list1, list2) => {
     // T: O(n+m) because worst case list1 and list2 are of different sizes
     // S: O(1) bc we are not creating new nodes
     
-    let mergedList = new ListNode();
-    let head = mergedList;
+    let mergedList = new ListNode(); // the new merged sorted linked list
+    let head = mergedList; // to save the head of the merged linked list
     
+    // while list1 and list2 has valid nodes
     while (list1 && list2) {
         if (list1.val < list2.val) {
             mergedList.next = list1;
@@ -28,11 +29,13 @@ const mergeTwoLists = (list1, list2) => {
         }
         mergedList = mergedList.next;
     }
+    // while loop ends when either list1 or list2 run out of elements
     
+    // if list1 still has nodes left over just add the rest of the nodes into the merged linked list
     if (list1) {
         mergedList.next = list1;
     }
-    
+    // if list2 still has nodes left over just add the rest of the nodes into the merged linked list
     if (list2) {
         mergedList.next = list2;
     }
