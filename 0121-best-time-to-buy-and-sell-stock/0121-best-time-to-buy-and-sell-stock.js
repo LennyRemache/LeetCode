07bf686c9449a1863e66a417c0e022d51cb995f3
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-02 21:36:48 -0500

    Time: 138 ms (58.28%), Space: 51.7 MB (51.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 const maxProfit = (prices) => {
+    // T: O(n)  S: O(1)
     let maxProfit = 0;
     let boughtPrice = prices[0];
     
     for (let i = 1; i < prices.length; i++) {
+        // if we potentially did not buy at the lowest price to produce most profit
         if (prices[i] < boughtPrice) boughtPrice = prices[i];
         maxProfit = Math.max(maxProfit, prices[i] - boughtPrice);
     }
