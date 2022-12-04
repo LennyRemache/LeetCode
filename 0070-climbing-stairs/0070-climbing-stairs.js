bad2705584b839368fe0a761f2c6654eed8cc1eb
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-04 18:31:24 -0500

    Time: 86 ms (62.10%), Space: 41.7 MB (76.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 const climbStairs = (n) => {
-    let memo = [1, 1, 2]; // base case for the fib sequence
+    let memo = [1,1,2];
     
-    for(let i = 3; i <= n; i++) {
+    for (i = 3; i <= n; i++) {
         memo.push(memo[i-1] + memo[i-2]);
     }
     
-    return memo[n];
+    return memo[n]
 };
