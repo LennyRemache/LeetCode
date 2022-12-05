3bdf9854b421ffbed084c1ac89e3b6acd8e57bfe
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-04 20:34:53 -0500

    Time: 63 ms (97.78%), Space: 42.8 MB (64.15%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -4,28 +4,32 @@
  * @return {string}
  */
 const addBinary = (a, b) => {
-    let res = '';
+    // T: O(n) where n represents the binary number with the max length
+    // S: O(n) where m represents the length of the summed binary number string
+    let res = [];
     let carryOver = 0;
     
+    // loop the length of the max length binary number
     for (let i = 0; i < Math.max(a.length, b.length); i++) {
+        // ternary operators to check if each string goes out of bounds -> out of bounds is falsy
         const digitA = a[a.length - i - 1] ? a[a.length - i - 1] : 0;
         const digitB = b[b.length - i - 1] ? b[b.length - i - 1] : 0;
         
         let sum = parseInt(digitA) + parseInt(digitB) + carryOver;
-        if (sum === 2) {
+        if (sum === 2) { // means "1" + "1" + "0" which should sum to 0 in binary and have a carryover of 1
             sum = 0;
             carryOver = 1;
-        } else if (sum === 3) {
+        } else if (sum === 3) { // means "1" + "1" + "1" which should sum to 1 in binary and have a carryover of 1
             sum = 1;
             carryOver = 1;
-        } else {
+        } else { // else means we have "0" + "1" + "0" or "1" + "0" + "0" which sums to 1 in binary and has carryover of 0
             carryOver = 0;
         }
         
-        res += sum.toString();
+        res.push(`${sum}`);
     }
     
-    if (carryOver === 1) res += "1";
+    if (carryOver === 1) res.push('1');
     
-    return res.split("").reverse().join("");
+    return res.reverse().join("");
 };
