0cf1af0bf93c9cf0e60af1860acf510dadd352af
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-05 18:28:44 -0500

    Time: 108 ms (67.76%), Space: 44.1 MB (67.35%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,34 +4,24 @@
  * @return {number[][]}
  */
 const insert = (intervals, newInterval) => {
-    let res = []; // the returned intervals with no overlaps with the newInterval
+    let res = [];
     
     let start = 0;
     let end = 1;
     
     let i = 0;
-    
-    // while there is no intersection between current interval and the new interval
-    // and while we have not reached a point where newIterval values can be inserted correctly
-    // and while we have not reach an interval greater than newInterval without intersection if applicable
-    while (i < intervals.length && !(intervals[i][end] >= newInterval[start])) {
+    while (i < intervals.length && intervals[i][end] < newInterval[start]) {
         res.push(intervals[i]);
         i++;
     }
     
-    // if prior loop ends before i reaches length of intervals array, that means we reached an intersection area
-    // in other words the end value of the current interval is >= the start value of the newInterval.
-    // so this loop will check that as long as we do not reach an interval that has a start value greater than the end of the newInterval
-    // in other words an interval that is greater and does not intersect with newInterval
-    // this will modify the newInterval values with the merged start and end values
-    while (i < intervals.length && !(intervals[i][start] > newInterval[end])) {
-        newInterval[start] = Math.min(intervals[i][start], newInterval[start]);
-        newInterval[end] = Math.max(intervals[i][end], newInterval[end]);
+    while(i < intervals.length && !(intervals[i][start] > newInterval[end])) {
+        newInterval[start] = Math.min(newInterval[start], intervals[i][start]);
+        newInterval[end] = Math.max(newInterval[end], intervals[i][end]);
         i++;
     }
     
-    res.push(newInterval); // before pushing the rest of the non intersecting intervals
-                           // push the merged interval if merged, otherwise just the original newInterval
+    res.push(newInterval);
     
     while (i < intervals.length) {
         res.push(intervals[i]);
