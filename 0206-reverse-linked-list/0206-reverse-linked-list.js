7ab8b8022ffa938729a6a4ad358bb4e30e6aa787
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-04 19:22:55 -0500

    Time: 74 ms (84.93%), Space: 43.8 MB (84.03%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -15,11 +15,13 @@ const reverseList = (head) => {
     let nextNode = null;
     
     while(currNode){
-        prevNode = currNode.next;
-        currNode.next = nextNode;
-        nextNode = currNode;
-        currNode = prevNode;
+        // 1. Reversing the linked list connection
+        prevNode = currNode.next; // save the node after for a safe disconnection
+        currNode.next = nextNode; // now we can safely break the connection and assign it the new next node that reverses the linked list here
+        
+        // 2. Preparing for the next iteration of reversing the linked list
+        nextNode = currNode; // setting up for the next connection that that the node we disconnect from will connect to
+        currNode = prevNode; // getting access back to the prev node we lost connection of which we will reverse its connection next
     }
-    
     return nextNode;
 };
