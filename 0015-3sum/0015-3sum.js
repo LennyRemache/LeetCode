0b94068f7a7af82590bed125df6c64b5413feeed
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-06 18:27:05 -0500

    Time: 140 ms (93.87%), Space: 53.2 MB (34.96%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -16,14 +16,12 @@ const threeSum = (nums) => {
         // if currNum = prevNum, we do not have to find currNum's triplets again.
         if(nums[i] === nums[i-1]) continue;
         
-        // 
-        //let subTarget = nums[i] * -1;
-        
         let left = i+1;              // j
         let right = nums.length - 1; // k
         
         while (left < right) {
-            if (nums[i] + nums[left] + nums[right] === 0) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum === 0) {
                 triplets.push([nums[i], nums[left], nums[right]]);
                 // increment left and decrement right in order to prevent duplicates
                 left++;
@@ -37,20 +35,20 @@ const threeSum = (nums) => {
                 // if nums[right] = nums[right+1], we do not have to find nums[right]'s triplets again.
                 while(left < right && nums[right] === nums[right+1]) right--;
                 
-            } else if (nums[i] + nums[left] + nums[right] < 0) {
+            } else if (sum < 0) {
                 // bc nums is sorted, increasing left pointer increases the sum by getting a larger number 
-                // on the left side so we get closer to subTarget
+                // on the left side so we get closer to 0
                 left++;
-            } else if (nums[i] + nums[left] + nums[right] > 0) {
+            } else if (sum > 0) {
                 // bc nums is sorted, decreasing right pointer decreases the sum by getting a smaller number 
-                // on the right side so we get closer to subTarget
+                // on the right side so we get closer to 0
                 right--;
             }
             
         }
-        //.      T      L  R
+        //.      i      L  R
         // [-4, -1, -1, 0, 1, 2]
-        // subTarget = 1     0 + 1 = 1
+        //                     -1 + 0 + 1 = 1
         // triplets = [[-1,-1,2], [-1,0,1]]
         
     }
