8bf8056e40a55c2c42b2365fc7ac558208653284
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-06 17:52:06 -0500

    Time: 128 ms (98.54%), Space: 52.5 MB (67.81%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -11,24 +11,28 @@ const threeSum = (nums) => {
         if(nums[i] === nums[i-1]) continue;
         
         let subTarget = nums[i] * -1;
-        let left = i + 1;
-        let right = nums.length - 1;
+        
+        let left = i+1; // j
+        let right = nums.length - 1; // k
         
         while (left < right) {
             if (nums[left] + nums[right] === subTarget) {
-                triplets.push([nums[i],nums[left], nums[right]]);
+                triplets.push([nums[i], nums[left], nums[right]]);
                 left++;
                 right--;
-                while (left < right && nums[left-1] === nums[left]) left++;
-                while (left < right && nums[right+1] === nums[right]) right--;
+                while(left < right && nums[left] === nums[left-1]) left++;
+                while(left < right && nums[right] === nums[right+1]) right--;
             } else if (nums[left] + nums[right] < subTarget) {
                 left++;
             } else if (nums[left] + nums[right] > subTarget) {
                 right--;
             }
+            
         }
-    }
+        //.      T   L        R
+        // [-4, -1, -1, 0, 1, 2]
+        // subTarget = 1     -1 + 2 = 1
         
-    
+    }
     return triplets;
 };
