06a0b05546814cb4fc923ae24509aa15d0df0796
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-05 19:43:32 -0500

    Time: 301 ms (60.67%), Space: 55.7 MB (57.52%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -3,6 +3,10 @@
  * @return {number[][]}
  */
 const updateMatrix = (mat) => {
+    // T: O(n*m + n*m) bc the double for loops depend on the size of the matrix argument + the while loop depends on worse case queue size
+    // S: O(n*m) worse case mat is all zeros so queue gets pushed with all the positions of each 0 in the matrix
+    
+    
     // BFS always paired with the usage of queues
     // bfs prevents revisits of the same cells in the matrix
     let queue = [];
@@ -11,8 +15,8 @@ const updateMatrix = (mat) => {
     for (let i = 0; i < mat.length; i++) {
         for (let j = 0; j < mat[0].length; j++) {
             // push into queue the row and col position and lastly a 0 to help calculate distances to nearest 0 when modified when trraversing the matrix/graph
-            if (mat[i][j] === 0) queue.push([i,j, 0]);
-            else mat[i][j] = -1;
+            if (mat[i][j] === 0) queue.push([i,j]);
+            else mat[i][j] = -1; // used to signal we reach a cell that needs to find its closest 0 distance
         }
     }
 
@@ -27,6 +31,10 @@ const updateMatrix = (mat) => {
         //const distance = pos[2]; // index 2 contains distance value currently to reach the closest 0;
         // ex: if curr cell from queue is 0 the distance is 0
         
+        /* if the dequeued cell is -1 then we can modify its value in the matrix to whatever the distance to the closest 0 is
+        if (mat[row][col] === -1) {
+            mat[row][col] = distance;
+        }*/
        
 
         // helper function to traverse in all 4 directions of curr cell that was dequeued from the queue
