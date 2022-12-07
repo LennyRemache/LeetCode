cbe329d98f7fdba46dab56cd6797f3843b9c3498
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-06 20:43:54 -0500

    Time: 123 ms (38.79%), Space: 45.4 MB (43.50%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -17,16 +17,16 @@ const evalRPN = (tokens) => {
         if (validOps[token] === undefined) {
             numStack.push(parseInt(token));
         } else {
-            num1 = numStack.pop();
             num2 = numStack.pop();
+            num1 = numStack.pop();
             if (token === "+") {
                 numStack.push(num1+num2);
             } else if(token === "-") {
-                numStack.push(num2-num1);
+                numStack.push(num1-num2);
             } else if(token === "*") {
                 numStack.push(num1*num2);
             } else if(token === "/") {
-                let res = num2/num1;
+                let res = num1/num2;
                 if(res > 0) numStack.push(Math.floor(res));
                 else numStack.push(Math.ceil(res))
             }
