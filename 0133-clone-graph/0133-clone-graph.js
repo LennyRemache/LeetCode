d060154dabad76ff09198da47983b678a516879d
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-06 20:21:49 -0500

    Time: 68 ms (92.27%), Space: 44 MB (31.80%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -11,27 +11,33 @@
  * @return {Node}
  */
 const cloneGraph = (node) => {
-    console.log(node)
+    // T: O(n + e) where n is the number of nodes and e is the number of edges
+    // T: O(v) where v is number values in visited hashMap
+    
     if(!node) return node;
     
     // used to check if we already made a clone of the visiting node
     let visited = {};
-    const clone = (node) => {
+    const createClone = (node) => {
         // base case
+        // if we already visited the current node, then that means we already made a clone of it
+        // so we can just return the clone stored in our visited hashMap
         if (visited[node.val] !== undefined) {
             return visited[node.val];
         }
         
+        // else we havent visited the current node so we make a copy and store it in the visited hashMap
         let copy = new Node(node.val);
         visited[node.val] = copy;
         
         // recurrence relation
+        // now for each copy/clone we need to add each of its neighbors which have to be clones aswell
         for(let neighbor of node.neighbors) {
-            copy.neighbors.push(clone(neighbor));
+            copy.neighbors.push(createClone(neighbor));
         }
         
         return copy;
     }
     
-    return clone(node);
+    return createClone(node);
 };
