ad52017b80356070bfdc133be03ae3d186cecdce
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-08 17:23:05 -0500

    Time: 154 ms (73.14%), Space: 45.8 MB (86.29%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -8,13 +8,18 @@
 const coinChange = (coins, amount) => {
     // T: O(c * a) => num of coins * length of table
     // S: O(a)
+    
+    // amount values are used as indexes
     const table = new Array(amount + 1).fill(Infinity); // tabulation always need an array of size n+1 where n represents the target to reach from 0
     
     table[0] = 0; // {amount: # of coins}
     
+    // loop through each possible amount up to the max amount given, so 0 -> amount
     for(let amount = 0; amount < table.length; amount++) {
+        // for each amount, find the minimum amount of coins needed to reach the current amount value
+        // based on using each of the coins available
         for(let coin of coins) {
-            if (coin <= amount) {
+            if (amount - coin >= 0) {
                 table[amount] = Math.min(table[amount], table[amount - coin] + 1)
             }
         }
