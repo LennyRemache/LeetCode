9ee44d144aba997094cc79bbe6a60506cbb9b1b7
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-08 17:36:47 -0500

    Time: 149 ms (77.29%), Space: 45.6 MB (90.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -5,13 +5,14 @@
  */
 
 // DP Tabulation => bottom-up approach for this problem is most optimal
-const coinChange = (coins, amount) => {
+const coinChange = (coins, target) => {
     // T: O(c * a) => num of coins * length of table
-    // S: O(a)
+    // S: O(a) -> length of tabular table
     
     // amount values are used as indexes
-    const table = new Array(amount + 1).fill(Infinity); // tabulation always need an array of size n+1 where n represents the target to reach from 0
+    const table = new Array(target + 1).fill(Infinity); // tabulation always need an array of size n+1 where n represents the target to reach from 0
     
+    // base case
     table[0] = 0; // {amount: # of coins}
     
     // loop through each possible amount up to the max amount given, so 0 -> amount
@@ -19,13 +20,20 @@ const coinChange = (coins, amount) => {
         // for each amount, find the minimum amount of coins needed to reach the current amount value
         // based on using each of the coins available
         for(let coin of coins) {
+            // check if the coin can even be used, if a coin results in geting a negative number then the coin is too large to use
             if (amount - coin >= 0) {
+                // set the # of coins for current amount value
+                // to the min between what is already stored at current amount value 
+                // and the # of coins at amount-currCoin value + 1
                 table[amount] = Math.min(table[amount], table[amount - coin] + 1)
             }
         }
     }
     
-    return table[amount] === Infinity ? -1 : table[amount];
+    // if our targetted value is still Infinity then that means it was never possible to 
+    // use the coins to reach the target, thus return -1.
+    // Otherwise we can return the # of coins stored at that value in the table.
+    return table[target] === Infinity ? -1 : table[target];
 };
 
 // DP Memoization(cache) => top-down approach is another type of DP
