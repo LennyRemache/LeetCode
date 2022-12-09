7cd38ebfae0a1fe43dd3883082bc9babb2e69320
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-08 19:03:39 -0500

    Time: 176 ms (64.35%), Space: 53.2 MB (83.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,17 +3,21 @@
  * @return {number[]}
  */
 const productExceptSelf = (nums) => {
+    // T: O(n)
+    // S: O(n)
     let sums = new Array(nums.length).fill(1);
     
     let left = new Array(nums.length).fill(1);
     let right = new Array(nums.length).fill(1);
     
-    left[0] = nums[0]
+    left[0] = nums[0] // handles first element not having anything to its left
+    // which is why we also start at index 1 since index 0 is already handled
     for(let i = 1; i < nums.length; i++) {
         left[i] = left[i-1] * nums[i];
     }
     
-    right[nums.length-1] = nums[nums.length-1]
+    right[nums.length-1] = nums[nums.length-1] // handles last element not having anything to its right
+    // which is why we also start at nums.length - 2 since index nums.length - 1 is already handled
     for(let i = nums.length - 2; i > 0; i--) {
         right[i] = right[i+1] * nums[i];
     }
