04ca538e6ac3f444ca402db834dc2d299fcaef0a
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-09 18:25:17 -0500

    Time: 254 ms (5.22%), Space: 51.1 MB (5.03%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -12,11 +12,15 @@
  */
 const isValidBST = (root) => {
     const dfs = (node, left, right) => {
-        if (!node) return true;
-                
+        if (!node) return true; // a null node is a valid BST 
+        console.log(`${left} < ${node.val} < ${right}`);
+        // if node.val is not within the valid range of values then it is not valid bst
         if (!(left < node.val && node.val < right)) {
             return false;
         }
+        
+        // having left and right ranges prevent subtree nodes from going beyond valid ranges
+        // that take into account previous node values and its ranges as well
         
         return dfs(node.left, left, node.val) && dfs(node.right, node.val, right);
     }
