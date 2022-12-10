ead402dd80989e6c4c461e3812fee831c0b0a08f
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-09 21:05:41 -0500

    Time: 133 ms (60.98%), Space: 56 MB (16.47%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,50 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+
+const numIslands = (grid) => {
+    let islands = 0;
+    let visited = new Set();
+    
+    const dfs = (row, col) => {  
+        let stack = [];
+        stack.push([row,col]);
+        
+        while (stack.length > 0) {
+            let cell = stack.pop();
+            let [r, c] = cell;
+            
+            const dir = [[-1,0], [1,0], [0,-1], [0,1]];
+            
+            for(let d of dir) {
+                const newRow = r + d[0];
+                const newCol = c + d[1];
+                
+                if (0 <= newRow && newRow < grid.length && 0 <= newCol && newCol < grid[0].length) {
+                    if (grid[newRow][newCol] === "1" && !visited.has(`${newRow},${newCol}`)) {
+                        visited.add(`${newRow},${newCol}`);
+                        stack.push([newRow, newCol]);
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    for (let i = 0; i < grid.length; i++) {
+        for(let j = 0; j < grid[0].length; j++) {
+            // if curr cell has not been visited and it is the start of an island
+            if (!visited.has(`${i},${j}`) && grid[i][j] === "1") {
+                visited.add(`${i},${j}`); // add the start of the island to visited
+                //console.log(visited)
+                dfs(i,j); // explore the entire island starting at curr cell while keeping track of cells that are part of that island
+                islands += 1;
+            } 
+            // else curr cell is water or is already a part of an island we visited before
+        }
+    }
+    
+    return islands;
+};
