e2db925b7a8febc9d80f1e06cb5cbd171cae3ced
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-10 18:41:50 -0500

    Time: 87 ms (85.72%), Space: 45 MB (57.52%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -3,45 +3,44 @@
  * @return {number}
  */
 const orangesRotting = (grid) => {
+    // T: O(n*m) since we are traversing the grid
+    // S: O(n*m) since we are also using a queue which worse case could 
     let queue = [];
     let numFresh = 0;
-    let time = 0;
-    
-    const bfs = () => {
-        const dir = [[-1,0],[1,0],[0,-1],[0,1]];
-
-        while (queue.length > 0) {
-            let qLen = queue.length;
-            for(let k = 0; k < qLen; k++) {
-                let [r, c] = queue.shift();
-                
-                dir.forEach((d) => {
-                    const newRow = r + d[0];
-                    const newCol = c + d[1];
-                    
-                    if (0 <= newRow && newRow < grid.length && 0 <= newCol && newCol < grid[0].length && grid[newRow][newCol] === 1) {
-                        queue.push([newRow, newCol]);
-                        grid[newRow][newCol] = -1; // marking the orange as being rot by another rotten orange
-                        numFresh -= 1;
-                    }
-                })
-            }
-            if(queue.length > 0) time += 1;
-        }
-    }
     
     for (let i = 0; i < grid.length; i++) {
         for(let j = 0; j < grid[0].length; j++) {
             // if cell is a starting rotten orange
             if (grid[i][j] === 2) {
                 queue.push([i,j]);
-            } else if (grid[i][j] === 1) {
+            } else if (grid[i][j] === 1) { // keep track of total fresh oranges in order to check in the end 
+                                           // if all fresh oranges were rotted
                 numFresh += 1;
             }
         }
     }
     
-    bfs();
+    const dir = [[-1,0],[1,0],[0,-1],[0,1]];
+    // Start to rot the oranges and keep track of time passed
+    let time = 0;
+    while (queue.length > 0) {
+        let qLen = queue.length;
+        for(let k = 0; k < qLen; k++) {
+            let [r, c] = queue.shift();
+
+            dir.forEach((d) => {
+                const newRow = r + d[0];
+                const newCol = c + d[1];
+
+                if (0 <= newRow && newRow < grid.length && 0 <= newCol && newCol < grid[0].length && grid[newRow][newCol] === 1) {
+                    queue.push([newRow, newCol]);
+                    grid[newRow][newCol] = -1; // marking the orange as being rot by another rotten orange
+                    numFresh -= 1;
+                }
+            })
+        }
+        if(queue.length > 0) time += 1;
+    }
         
     return numFresh > 0 ? -1 : time ;        
 };
