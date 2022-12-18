3d9f2cf77090d604b2820a4532e694221139cd30
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-18 17:44:44 -0500

    Time: 2499 ms (71.19%), Space: 80.8 MB (79.63%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
@@ -0,0 +1,59 @@
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.endOfWord = false;
+    }
+}
+
+class WordDictionary {
+    constructor() {
+        this.trie = new TrieNode();
+    }
+    
+    addWord(word) {
+        let node = this.trie;
+        
+        for(let letter of word) {
+            if (node.children[letter] === undefined) {
+                node.children[letter] = new TrieNode();
+            }
+            node = node.children[letter];
+        }
+        node.endOfWord = true;
+    }
+    
+    search(word) {
+        
+        const dfs = (j, root) => {
+            let node = root;
+        
+            for(let i = j; i < word.length; i++) {
+                let letter = word[i];
+                if (letter === ".") {
+                    for (let child of Object.values(node.children)) {
+                        //console.log(child)
+                        if (dfs(i+1, child)) return true;
+                    }
+                    return false;
+                } else {
+                    if (node.children[letter] === undefined) {
+                        return false;
+                    }
+                    node = node.children[letter];    
+                } 
+                
+            }   
+            
+            return node.endOfWord;
+        }
+        
+        return dfs(0, this.trie);
+    }
+};
+
+/** 
+ * Your WordDictionary object will be instantiated and called as such:
+ * var obj = new WordDictionary()
+ * obj.addWord(word)
+ * var param_2 = obj.search(word)
+ */
