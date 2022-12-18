2a2d9c526b2d2c0527375d0f94371061e76c17ea
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-17 20:19:17 -0500

    Time: 823 ms (10.64%), Space: 126.5 MB (8.15%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number} capacity
+ */
+
+// a Map keeps track automatically of the order keys are set in the map
+var LRUCache = function(capacity) { // constructor
+    this.size = capacity;
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+
+// prototype used to add methods to the object type
+LRUCache.prototype.get = function(key) {
+    if (this.cache.has(key)) {
+        const val = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, val);
+        return val;
+    }
+    return -1;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    // using our own built get() method to check if the key exists already
+    // causes the map to reorder itself automatically based on LRU.
+    // So we do not need to reeorder the used key again in put() if it already
+    // exists in the cache.
+    if (this.get(key) === -1 && this.cache.size === this.size) {
+        // .keys() is an iterable object of keys
+        for(let k of this.cache.keys()) { // delete LRU key
+            this.cache.delete(k);
+            break;
+        }
+    } 
+    
+    this.cache.set(key, value);
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
