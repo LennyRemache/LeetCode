5983630a23e623e83ad73f393a8e806bd6ad2240
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-18 16:23:44 -0500

    Time: 364 ms (26.82%), Space: 63.8 MB (53.19%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -1,5 +1,4 @@
-// tree data structure
-// T: O(l * n) => average word length * # of words
+
 class TrieNode {
     constructor() {
         this.children = {};
@@ -7,83 +6,44 @@ class TrieNode {
     }
 }
 
-class Trie{
+class Trie {
     constructor() {
-        this.root = new TrieNode(); // initialize the root TrieNode of the trie data strcuture
-    }
-
-    /**
-     * @param {string} word
-     * @return {void}
-     */
-    insert(word) {
-        // start at root TrieNode
-        let currNode = this.root;
-        // check letter by letter of the word
-        for(let i = 0; i < word.length; i++) {
-            const letter = word[i];
-            // if currLetter of word is not an existing child of currNode then
-            // we can add the letter to currNode's children as a new TrieNode
-            if (currNode.children[letter] === undefined) {
-                currNode.children[letter] = new TrieNode();
-            } 
-            
-            // we can continue on to each following curr letter's TrieNode
-            currNode = currNode.children[letter];
-            
-        }
-        // when loops end we reached the last letter of a word so we can set 
-        // the final currNode's endOfWord state to true
-        currNode.endOfWord = true;
-    }
-
-    /** 
-     * @param {string} prefix
-     * @return {boolean}
-     */
-    search(word) {
-        // start at root TrieNode
-        let currNode = this.root;
-        // check letter by letter of the word
-        for(let i = 0; i < word.length; i++) {
-            const letter = word[i];
-            // if currLetter of word is not an existing child of currNode then 
-            // we can conclude that the rest of the word does no exist in the Trie either
-            if (currNode.children[letter] === undefined) {
-                return false
-            } 
-            // we can continue on to each following curr letter's TrieNode
-            currNode = currNode.children[letter];
-            
-        }
-        // when loop ends we reached the last letter of the word
-        // we just have to return currNode's endOfWord state to determine if
-        // the word was inserted before or not
-        return currNode.endOfWord;
+        this.root = new TrieNode();
     }
     
-    /** 
-     * @param {string} prefix
-     * @return {boolean}
-     */
-    startsWith(prefix) {
-        // start at root TrieNode
-        let currNode = this.root;
-        // check letter by letter of the prefix
-        for(let i = 0; i < prefix.length; i++) {
-            const letter = prefix[i];
-            // if currLetter of prefix is not an existing child of currNode then 
-            // we can conclude that the rest of the prefix does not exist in the Trie either
-            if (currNode.children[letter] === undefined) {
+    insert(word) {
+        let node = this.root;
+        for (let letter of word) {
+            if (node.children[letter] === undefined) {
+                node.children[letter] = new TrieNode();
+            } 
+            node = node.children[letter];
+        }
+        
+        node.endOfWord = true;
+    }
+    
+    search(word) {
+        let node = this.root;
+        for (let letter of word) {
+            if (node.children[letter] === undefined) {
                 return false
             } 
-            // we can continue on to each following curr letter's TrieNode
-            currNode = currNode.children[letter];
-            
+            node = node.children[letter];
         }
-        // when loop ends we reached the last letter of the prefix
-        // we can return true since we found the prefix in the trie (the actual word containing that prefix does not matter)
-        // it also does not matter if the prefix is a word or not
+        
+        return node.endOfWord;
+    }
+    
+    startsWith(prefix) {
+        let node = this.root;
+        for (let letter of prefix) {
+            if (node.children[letter] === undefined) {
+                return false;
+            }
+            node = node.children[letter];
+        }
+        
         return true;
     }
 }
