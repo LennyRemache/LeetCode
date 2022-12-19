91cab380f95192394884597500baf49063f607ed
Lenny <107436863+LennyRemache@users.noreply.github.com>
2022-12-19 13:38:06 -0500

    Time: 86 ms (64.62%), Space: 43.4 MB (63.08%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.js
+++ 0284-peeking-iterator/0284-peeking-iterator.js
@@ -17,28 +17,33 @@
 /**
  * @param {Iterator} iterator
  */
+
+// can assume valid next() and peek() calls are made that do not exceed the iterators element count
 class PeekingIterator {
     constructor(iterator) {
         this.iterator = iterator;
-        this.cache = this.iterator.next(); // used for peeking
+        this.cache = this.iterator.next(); // used for caching the peeking element that changes as next() is called.
+        // cache assigned at class object initialization because it is possible to call peek() as first instruction
+        // so this prevents peek() from returning null as if we made an invalid call to peek()/reached end of iterator
     }
     
-    next() {
-        let res = this.cache; // previous peeked element
-        if(this.iterator.hasNext()) {
+    next() { // returns the cached element since technically is like calling next() twice when cache is initialized with next() at assignment
+        // so we want to return the value cached in the prior next() call rather than the current next() call
+        let res = this.cache; // previous peeked element from cache
+        if(this.iterator.hasNext()) { // if next is possible then then next cached peek element should also be updated
             this.cache = this.iterator.next();
-        } else {
+        } else { // else then there should not be a peeking element anymore
             this.cache = null;
         }
-        return res;
+        return res; // return the previous peeking element since
     }
     
     hasNext() {
-        return this.cache !== null;
+        return this.cache !== null; // unless the cached peek element is null then there must be a next element
     }
     
     peek() {
-        return this.cache;
+        return this.cache; // simply return the cached peeked element
     }
 };
 
