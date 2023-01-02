bc428a8b457ed0ea1ce79e38c323e63ae3a3094c
Lenny <107436863+LennyRemache@users.noreply.github.com>
2023-01-02 18:48:32 -0500

    Time: 124 ms (5.15%), Space: 41.8 MB (80.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -6,28 +6,22 @@ const sortColors = (nums) => {
     // T: O(n) where n is length of nums
     // S: O(1) since numCount will only have 0,1,2 as possible keys
     
-    // similar to bucket sorting
-    let numCount = {
-        0: 0, 
-        1: 0,
-        2: 0
-    };
-    
-    for(let num of nums) { // O(n)
-        numCount[num] += 1;
-    }
-    
     let i = 0;
-    let color = 0;
-    while (i < nums.length) { // O(n)
-        if (numCount[color] === 0) {
-            color += 1;
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (i <= right) {
+        if (nums[i] === 0) {
+            nums[i] = nums[left];
+            nums[left] = 0;
+            left++;
+        } else if (nums[i] === 2) {
+            nums[i] = nums[right];
+            nums[right] = 2;
+            right--;
             continue;
         }
-        nums[i] = color;
-        numCount[color] -= 1;
-
-        i++
+        i++;
     }
     
     return nums;
