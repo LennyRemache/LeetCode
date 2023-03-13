eea8fae738b4e1830360f9402bcd5e96d822112d
Lenny <lenny.j.remache@gmail.com>
2023-03-13 19:06:59 -0400

    Time: 90 ms (50.04%), Space: 52.1 MB (11.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,15 +2,29 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// T: O(n) since we loop through the entire prices array once
+// S: O(1) since we do not use additional data structures that depend on the size of prices array
+
 const maxProfit = (prices) => {
-    // T: O(n)  S: O(1)
+    // you buy it the first day
+    let buyPrice = prices[0];
+    // you have not sold yet so profit is 0
     let maxProfit = 0;
-    let boughtPrice = prices[0];
     
-    for (let i = 1; i < prices.length; i++) {
-        // if we potentially did not buy at the lowest price to produce most profit
-        if (prices[i] < boughtPrice) boughtPrice = prices[i];
-        maxProfit = Math.max(maxProfit, prices[i] - boughtPrice);
+    // loop through the rest of the array
+    for(let i = 1; i < prices.length; i++) {
+        // curr price is a possible day to sell the stock at a profit or loss
+        let sellPrice = prices[i];
+        
+        // check what the profit is and see if it is the new maxProfit
+        maxProfit = Math.max(maxProfit, sellPrice - buyPrice);
+        
+        // if profit is negative then we found a day where we couldve instead bought for cheaper
+        if (sellPrice - buyPrice < 0) {
+            buyPrice = sellPrice;
+        }
+        
     }
     
     return maxProfit;
