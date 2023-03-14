36d554466d95523d12b84ecae1a9f0a36e8838b4
Lenny <lenny.j.remache@gmail.com>
2023-03-13 20:02:50 -0400

    Time: 82 ms (44.17%), Space: 44.4 MB (74.34%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,33 +2,45 @@
  * @param {string} s
  * @return {boolean}
  */
+
+// T: O(n) since worse case we loop through the entire input string
+// S: O(1) since we do not use any additional data structure that depends on the size of the input string
+
 var isPalindrome = function(s) {
-    s = s.toLowerCase();
+    s = s.toLowerCase(); // chanege entire string to lowercase letters
     
-    let left = 0;
-    let right = s.length - 1;
+    let left = 0; // the left pointer
+    let right = s.length - 1; // the right pointer
     
+    // loop only while left pointer does not overlap right pointer
     while (left < right) {
+        // conditions to check if the character at left and right pointer are letters or numbers
         let isLeftAlpha = s.charCodeAt(left) >= 97 && s.charCodeAt(left) <= 122;
         let isLeftNum = s.charCodeAt(left) >= 48 && s.charCodeAt(left) <= 57;
         
         let isRightAlpha = s.charCodeAt(right) >= 97 && s.charCodeAt(right) <= 122;
         let isRightNum = s.charCodeAt(right) >= 48 && s.charCodeAt(right) <= 57;
         
+        // if left is not a letter or number then we can move to next char
         if (!isLeftAlpha && !isLeftNum) {
             left++;
             continue;
         }
+        // if right is not a letter or number then we can move to next char
         if (!isRightAlpha && !isRightNum) {
             right--;
             continue;
         }
         
+        // reach here when both left and right are letters or numbers so we check if they are identical
         if (s[left] !== s[right]) return false;
         
+        // reach here if the letters or numbers are identical on the left and right
+        // now we safely increment/decrement our pointers
         left++;
         right--;
     }
     
+    // loop only ends when the string is a valid palindrome
     return true;
 };
