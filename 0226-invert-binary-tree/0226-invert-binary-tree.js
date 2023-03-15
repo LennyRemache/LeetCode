69f60195c2dea09766abdc0568c7f7691ae165ac
Lenny <lenny.j.remache@gmail.com>
2023-03-15 15:03:57 -0400

    Time: 54 ms (94.02%), Space: 42.2 MB (69.65%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -10,13 +10,20 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+
+// T: O(n) since we are travelling through all the nodes once
+// S: O(1) since we do not create any additional data structure. We invert the binary tree in place rather than creating a new binary tree
+
 const invertTree = (root) => {
-    
+    // base case to end recursion when node reached is null
     if (!root) return root;
     
+    // traverse as far left as possible
     let left = invertTree(root.left);
+    // then travel to the right of the current node
     let right = invertTree(root.right);
     
+    // simply swap the current found left and right nodes that are children of the same parent node
     root.left = right;
     root.right = left;
     
