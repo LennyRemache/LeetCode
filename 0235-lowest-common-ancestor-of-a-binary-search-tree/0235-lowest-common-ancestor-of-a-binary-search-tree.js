b1dd121d9df12a75f59fc7d01cc6e21706463896
Lenny <lenny.j.remache@gmail.com>
2023-03-15 16:51:57 -0400

    Time: 74 ms (94.55%), Space: 51.9 MB (67.69%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -12,10 +12,13 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+
+// T: O(h) where h is the height of the binary search tree
+// S: O(1) since we are not creating any additional data structure. We are just searching in place 
 const lowestCommonAncestor = (root, p, q) => {
     while (root) {
         if (p.val < root.val && q.val < root.val) root = root.left;
         else if (p.val > root.val && q.val > root.val) root = root.right;
-        else return root
+        else return root;
     }
 };
