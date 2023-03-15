a57b094b5ffedde6817a8a9848e2a67a88d0a88a
Lenny <lenny.j.remache@gmail.com>
2023-03-15 15:30:04 -0400

    Time: 62 ms (83.75%), Space: 45.5 MB (10.58%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,20 +4,25 @@
  * @return {number}
  */
 const search = (nums, target) => {
+    // pointers to help find the mid point of the array
     let left = 0;
     let right = nums.length - 1;
     
+    // loop to continously slice the array in half 
+    // since array is sorted it is safe to cut array in halves until we reach the target
     while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (nums[mid] === target) return mid;
+        let mid = Math.floor((left + right) / 2); // get the middle index
+        if (nums[mid] === target) return mid; // middle index contains target value
         
-        if (nums[mid] > target) {
+        if (nums[mid] > target) { // if middle index contains larger value than target then we can ignore everything to the right of it since everything to the right of it would also be greater than the target when sorted
             right = mid - 1;
         }
-        if (nums[mid] < target) {
+        else if (nums[mid] < target) {
+            // if middle index contains smaller value than target then we can ignore everything to the left of it since everything to the left of it would also be less than the target when sorted
             left = mid + 1;
         }
     }
     
+    // if we reach this point then we never found the target
     return -1;
 };
