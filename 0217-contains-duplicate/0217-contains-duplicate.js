799ebda2d1aeff35d32aa92a1eaea4d1c5da57ef
Lenny <lenny.j.remache@gmail.com>
2023-04-04 15:13:56 -0400

    Time: 104 ms (44.44%), Space: 53.3 MB (69.53%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,22 +3,15 @@
  * @return {boolean}
  */
 const containsDuplicate = (nums) => {
-    /*
-    nums.sort((a,b) => a-b); // time is O(nlog(n)) however now space is O(1)
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i-1]) return true;
+    
+    let numCount = {};
+    
+    for(let num of nums) {
+        if (numCount[num] === undefined) numCount[num] = 0;
+        numCount[num] += 1;
+        
+        if (numCount[num] >= 2) return true;
     }
     
-    return false;
-    */
-    
-    const numFreq = {}; // T: O(n) and S: O(n)
-    
-    for (let num of nums) {
-        if (numFreq[num] === undefined) numFreq[num] = 1;
-        else return true;
-    }
-    
-    return false;
-
+    return false
 };
