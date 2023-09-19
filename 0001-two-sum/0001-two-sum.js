1aa3f511eb3a6967c8e24a2a7036a63a339c121f
Lenny <lenny.j.remache@gmail.com>
2023-09-19 15:51:02 -0400

    Time: 63 ms (63.12%), Space: 42.7 MB (42.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,25 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
-
-// T: O(n) worse case since at most we loop the entire nums array once
-// S: O(n) worse case since at most we loop through the entire nums array and store each complement in the dictionary
-
 const twoSum = (nums, target) => {
-    let subTarget = {}; // dictionary has constant O(1) lookup time
-                        // dictionary will hold all the complements of the current number at index i
-                        // complements are the subtargets that the currnum at i needs to find in the rest of the                         // nums array in order to sum to the target
+    // used to store the index of a previously seen num that could potentially be a match with future num
+    const pairs = {}; // worse case O(n) space where n is size of nums
     
-    // loop through the entire nums array once
-    for (let i = 0; i < nums.length; i++) {
-        // is curr num at index i a complement of a previous number seen in the nums array?
-        if (subTarget[nums[i]] !== undefined) {
-            return [subTarget[nums[i]], i];
-        } else {
-            // the subtarget that curr num needs to find
-            let complement = target - nums[i];
-            // store the subtarget and the index of currnum that needs to find the new subtarget created
-            subTarget[complement] = i;
+    // O(n) worse case runtime where n is the size of nums array
+    for(let i = 0; i < nums.length; i++) {
+        let currNum = nums[i]; // O(1)
+        // key is mathematically looking for if any num that matches with currNum to equal target was potentially seen already
+        // match = target - currNum ------> target =  currNum + match
+        const match = target - currNum;
+        // if the match for currNum was not found then we store currNum as a potential match for another num in future iterations
+        if (pairs[match] === undefined) {
+            pairs[currNum] = i; // we store the index of the saved num since we are later returning indices of a
+                                // valid pair of nums that add to target
+        } 
+        // if the match for currNum was seen before then we can return currNums index i and the stored index for match
+        else {
+            return [pairs[match], i]
         }
     }
 };
