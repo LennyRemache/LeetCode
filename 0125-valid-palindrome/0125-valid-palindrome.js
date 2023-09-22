3760820e3ec14dcb7373040c8b0fb827ea950936
Lenny <lenny.j.remache@gmail.com>
2023-09-22 17:43:34 -0400

    Time: 55 ms (89.28%), Space: 44.9 MB (67.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,45 +2,33 @@
  * @param {string} s
  * @return {boolean}
  */
-
-// T: O(n) since worse case we loop through the entire input string
-// S: O(1) since we do not use any additional data structure that depends on the size of the input string
-
-var isPalindrome = function(s) {
-    s = s.toLowerCase(); // chanege entire string to lowercase letters
+const isPalindrome = (s) => {
+    s = s.toLowerCase();
     
-    let left = 0; // the left pointer
-    let right = s.length - 1; // the right pointer
+    let left = 0;
+    let right = s.length - 1;
     
-    // loop only while left pointer does not overlap right pointer
     while (left < right) {
-        // conditions to check if the character at left and right pointer are letters or numbers
-        let isLeftAlpha = s.charCodeAt(left) >= 97 && s.charCodeAt(left) <= 122;
-        let isLeftNum = s.charCodeAt(left) >= 48 && s.charCodeAt(left) <= 57;
+        const isLeftAlpha = 97 <= s.charCodeAt(left) && s.charCodeAt(left) <= 122;
+        const isLeftNum = 48 <= s.charCodeAt(left) && s.charCodeAt(left) <= 57;
         
-        let isRightAlpha = s.charCodeAt(right) >= 97 && s.charCodeAt(right) <= 122;
-        let isRightNum = s.charCodeAt(right) >= 48 && s.charCodeAt(right) <= 57;
+        const isRightAlpha = 97 <= s.charCodeAt(right) && s.charCodeAt(right) <= 122;
+        const isRightNum = 48 <= s.charCodeAt(right) && s.charCodeAt(right) <= 57;
         
-        // if left is not a letter or number then we can move to next char
         if (!isLeftAlpha && !isLeftNum) {
-            left++;
-            continue;
+            left += 1;
         }
-        // if right is not a letter or number then we can move to next char
-        if (!isRightAlpha && !isRightNum) {
-            right--;
-            continue;
+        else if (!isRightAlpha && !isRightNum) {
+            right -= 1;
         }
-        
-        // reach here when both left and right are letters or numbers so we check if they are identical
-        if (s[left] !== s[right]) return false;
-        
-        // reach here if the letters or numbers are identical on the left and right
-        // now we safely increment/decrement our pointers
-        left++;
-        right--;
+        else if (s[left] !== s[right]) {
+            return false;
+        }
+        else {
+            left += 1;
+            right -= 1;
+        }
     }
     
-    // loop only ends when the string is a valid palindrome
     return true;
 };
