34997c2e726eb137f7e96185a849e398514714d4
Lenny <lenny.j.remache@gmail.com>
2023-09-22 16:56:36 -0400

    Time: 77 ms (35.77%), Space: 51.4 MB (68.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,18 +2,26 @@
  * @param {number[]} prices
  * @return {number}
  */
+
+// O(n) runtime where n is the length of prices arr
+// O(1) space as we are not storing any data in a data structure
 const maxProfit = (prices) => {
-    let buyPrice = prices[0];
-    let max = 0;
+    let buyPrice = prices[0]; // buy first day
+    let maxProfit = 0; // have not sold yet so profit is 0
     
+    // loop through each days price
     for(let i = 1; i < prices.length; i++) {
+        // curr price we are selling at
         const sellPrice = prices[i];
-        max = Math.max(max, sellPrice - buyPrice);
+        // if selling at curr price brings more profit, update maxprofit otherwise dont
+        const currProfit = sellPrice - buyPrice;
+        maxProfit = Math.max(maxProfit, currProfit);
         
-        if(sellPrice - buyPrice < 0) {
+        // if we are selling at a loss then that means curr day is a cheaper day to buy
+        if(currProfit < 0) {
             buyPrice = sellPrice;
         }
     }
     
-    return max;
+    return maxProfit;
 };
