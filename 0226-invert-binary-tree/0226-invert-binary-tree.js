b7b7df6de5a437ce13f11cb23ba9bc473a40e512
Lenny <lenny.j.remache@gmail.com>
2023-09-22 18:17:23 -0400

    Time: 52 ms (57.29%), Space: 42.1 MB (60.56%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -10,14 +10,19 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
+
+// O(n) runtime where n is the # of nodes in the binary tree
+// O(1) since we only use the provided binary tree data structure
 const invertTree = (root) => { 
-    if (!root) return root;
+    if (root === null) return root; // base case is when we find the deepest node
     
-    const left = invertTree(root.left);
-    const right = invertTree(root.right);
+    const left = invertTree(root.left); // retrieve the deepest left node currently
+    const right = invertTree(root.right); // retrieve the deepest right node currently
     
+    // swap after both curr deepest left and right nodes of the same parent are found
     root.left = right;
     root.right = left;
     
+    // retrieve the inverted sub binary tree until we invert the main inverted binary tree
     return root;
 };
