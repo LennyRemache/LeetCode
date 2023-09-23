3ccab1b4de4efafa74f88914ccff9e94142903a8
Lenny <lenny.j.remache@gmail.com>
2023-09-22 20:43:23 -0400

    Time: 63 ms (57.87%), Space: 44.5 MB (33.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -8,17 +8,20 @@
 const floodFill = (image, sr, sc, color) => {
     const startColor = image[sr][sc];
     
-    if(startColor === color) return image;
+    // if starting position already has the color we are suppose to change too then we do not need to move forward
+    if(startColor === color) return image; 
     
-    const dir = [[-1, 0], [1, 0], [0, -1], [0, 1]] // up, down, left, right
-    
+    // traverse through the grid using dfs
     const dfs = (r, c) => {
+        // conditions to check out of bound errors and if not out of bounds then check it matches the startColor
         if (r < 0 || r > image.length - 1 || c < 0 || c > image[0].length - 1 || image[r][c] !== startColor) {
             return;
         }
         
+        // only then can we modify the color
         image[r][c] = color;
         
+        // and then we can traverse adjacent to the current position
         dfs(r-1, c); // up
         dfs(r+1, c); // down
         dfs(r, c-1); // left
