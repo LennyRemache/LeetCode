27544e5263388b6dd19de57ba04bfe854775c502
Lenny <lenny.j.remache@gmail.com>
2023-09-24 15:20:59 -0400

    Time: 57 ms (87.26%), Space: 47.1 MB (60.38%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -10,25 +10,21 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-
-// T: O(n) since we travel through each node and its children at most once
-// S: O(1) since we do not create any additional data structure
-
 const isBalanced = (root) => {
-    let balanced = true; // used to keep track of which sections of the tree make the whole tree unbalanced or not
+    let balanced = true;
     
     const dfs = (node) => {
-        if (!node) return 0; // when a null node is reached then the height should be 0
+        if (node === null) return 0;
         
-        let leftHeight = dfs(node.left);
-        let rightHeight = dfs(node.right);
+        const leftHeight = dfs(node.left);
+        const rightHeight = dfs(node.right);
         
-        if(Math.abs(leftHeight-rightHeight) > 1) balanced = false; // balance is false when left height and right height have a difference of more than 1
-        
-        return Math.max(leftHeight, rightHeight) + 1; // the max height between left and right is the new height of the parent node + 1 since we are traversing back up the tree by height of 1
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            balanced = false;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
     }
-    
     dfs(root);
     
-    return balanced
+    return balanced;
 };
