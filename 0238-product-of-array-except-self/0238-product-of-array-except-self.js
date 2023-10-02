e61fd31995782995e84f90d58159aa2d72d90e14
Lenny <lenny.j.remache@gmail.com>
2023-10-02 19:15:43 -0400

    Time: 95 ms (36.80%), Space: 57.1 MB (9.30%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,19 +2,28 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+// O(n) runtime since each loop depends on nums.length
+// O(n) space since prefix and postfix depend on nums.length
+
 const productExceptSelf = (nums) => {
     
     const prefix = new Array(nums.length).fill(1);
+    // get prefix of all nums -> meaning product of all elements to the left of curr num
+    // by default the first element has a prefix of 1 since it has no elements to its left
     for(let i = 1; i < nums.length; i++) {
-        prefix[i] = prefix[i-1] * nums[i-1];
+        prefix[i] = prefix[i-1] * nums[i-1]; // curr num prefix = left num * the pre caclulated prefix of left num from prev iteration
     }
-    console.log(prefix)
+    
     const postfix = new Array(nums.length).fill(1);
+    // get postfix of all nums -> meaning product of all elements to the right of curr num
+    // by default the last element has a postfix of 1 since it has no elements to its right
     for(let i = nums.length - 2; i >= 0; i--) {
-        postfix[i] = nums[i+1] * postfix[i+1];
+        postfix[i] = nums[i+1] * postfix[i+1]; // curr num postfix = right num * the pre caclulated postfix of right num from prev iteration
     }
-    console.log(postfix)
+  
     const res = new Array(nums.length).fill(1);
+    // res is simply prefix * postfix
     for(let i = 0; i < res.length; i++) {
         res[i] = prefix[i] * postfix[i];
     }
