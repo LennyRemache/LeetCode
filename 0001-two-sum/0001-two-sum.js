63dc5524871df5d1b02bd2f714306c92d0b4fa86
Lenny <lenny.j.remache@gmail.com>
2023-10-01 22:02:21 -0400

    Time: 64 ms (58.93%), Space: 42.5 MB (51.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -8,16 +8,16 @@
 // O(n) space worse case where pairs is same size as nums
 
 const twoSum = (nums, target) => {
-    const pairs = {};
+    const prevVal = {};
     
     for(let i = 0; i < nums.length; i++) {
         const subTarget = target - nums[i]; // the number we are looking for to pair with curr nums[i]
         // if that other number is not found
-        if (pairs[subTarget] === undefined) {
+        if (prevVal[subTarget] === undefined) {
             // store current num as the key and its index as the value for future numbers to find it as its subTarget
-            pairs[nums[i]] = i; 
+            prevVal[nums[i]] = i; 
         } else {
-            return [pairs[subTarget], i]; // if that other number exists then a valid pair exists
+            return [prevVal[subTarget], i]; // if that other number exists then a valid pair exists
         }
     }
 };
