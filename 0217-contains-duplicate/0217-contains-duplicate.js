02e1c3b8013671ee2a48c6cd71692ec29c1cd356
Lenny <lenny.j.remache@gmail.com>
2023-10-01 20:19:22 -0400

    Time: 89 ms (39.45%), Space: 54 MB (59.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,16 +2,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// O(n) runtime where n is the size of nums
+// O(n) space worse case where all num of nums are unique and hashmap equals size of nums
 const containsDuplicate = (nums) => {
-    
-    let numCount = {};
+    const freq = {}; // track the frequency of each distinct integer
     
     for(let num of nums) {
-        if (numCount[num] === undefined) numCount[num] = 0;
-        numCount[num] += 1;
+        if (freq[num] === undefined) {
+            freq[num] = 0; // first time an integer appears we define its value in the hash map
+        }
+        freq[num] += 1; // add 1 to the frequency of curr num
         
-        if (numCount[num] >= 2) return true;
+        if (freq[num] >= 2) return true; // if frequency of curr num is 2 or more then it contains duplicate
     }
     
-    return false
+    return false; // no duplicate found if loop ends
 };
