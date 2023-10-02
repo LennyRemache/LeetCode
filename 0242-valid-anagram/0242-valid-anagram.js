4274ccbd16363b4388be8eb90ac16cf7fc5bc032
Lenny <lenny.j.remache@gmail.com>
2023-10-01 20:42:54 -0400

    Time: 57 ms (94.06%), Space: 43.6 MB (69.90%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,34 +4,23 @@
  * @return {boolean}
  */
 
-// O(n) runtime where n is length of strings
-// O(n) space where hashmap size is based on string size
 const isAnagram = (s, t) => {
-    
-    // base case mustr check if both strings have the same # of letters to be an anagram
     if (s.length !== t.length) return false;
     
-    // hashmap to keep track of unique letter occurrences
-    const letterCount = {};
+    const letterFreq = {};
     
-    // O(n) where is length of strings
-    // loop to count available characters in s that t can use
-    for(let letter of s) {
-        // if letter is new then we defined it in the hashmap to keep track of
-        if (letterCount[letter] === undefined) {
-            letterCount[letter] = 0;
+    for(const letter of s) {
+        if (letterFreq[letter] === undefined) {
+            letterFreq[letter] = 0;
         }
-        // increment letter occurrence count
-        letterCount[letter] += 1;
+        letterFreq[letter] += 1;
     }
-    
-    // O(n) where n is length of strings
-    // loop to check if all characters in s are used by t
-    for(let letter of t) {
-        // if character was not seen in s or was already used enough times by t
-        if (letterCount[letter] === undefined || letterCount[letter] <= 0) return false
-        // otherwise we can safely deduct the amount available of the current letter
-        letterCount[letter] -= 1;
+   
+    for(const letter of t) {
+        if(letterFreq[letter] === undefined || letterFreq[letter] === 0) {
+            return false;
+        }
+        letterFreq[letter] -= 1;
     }
     
     return true;
