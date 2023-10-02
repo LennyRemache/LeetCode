e86156f99d95508d48a4a7731bb9372cdb5e4879
Lenny <lenny.j.remache@gmail.com>
2023-10-01 21:47:58 -0400

    Time: 58 ms (72.32%), Space: 42.8 MB (38.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,23 +4,14 @@
  * @return {number[]}
  */
 const twoSum = (nums, target) => {
-    // used to store the index of a previously seen num that could potentially be a match with future num
-    const pairs = {}; // worse case O(n) space where n is size of nums
+    const pairs = {};
     
-    // O(n) worse case runtime where n is the size of nums array
     for(let i = 0; i < nums.length; i++) {
-        let currNum = nums[i]; // O(1)
-        // key is mathematically looking for if any num that matches with currNum to equal target was potentially seen already
-        // match = target - currNum ------> target =  currNum + match
-        const match = target - currNum;
-        // if the match for currNum was not found then we store currNum as a potential match for another num in future iterations
-        if (pairs[match] === undefined) {
-            pairs[currNum] = i; // we store the index of the saved num since we are later returning indices of a
-                                // valid pair of nums that add to target
-        } 
-        // if the match for currNum was seen before then we can return currNums index i and the stored index for match
-        else {
-            return [pairs[match], i]
+        const subTarget = target - nums[i];
+        if (pairs[subTarget] === undefined) {
+            pairs[nums[i]] = i;
+        } else {
+            return [pairs[subTarget], i];
         }
     }
 };
