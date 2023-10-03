71e597de8420d5957cfd04e14a67ea42551d0f18
Lenny <lenny.j.remache@gmail.com>
2023-10-03 16:51:02 -0400

    Time: 94 ms (39.60%), Space: 54.1 MB (62.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,29 +3,22 @@
  * @return {number[]}
  */
 
-// O(n) runtime since each loop depends on nums.length
-// O(n) space since prefix and postfix depend on nums.length
 
 const productExceptSelf = (nums) => {
+    const res = new Array(nums.length);
     
-    const prefix = new Array(nums.length).fill(1);
-    // get prefix of all nums -> meaning product of all elements to the left of curr num
-    // by default the first element has a prefix of 1 since it has no elements to its left
-    for(let i = 1; i < nums.length; i++) {
-        prefix[i] = prefix[i-1] * nums[i-1]; // curr num prefix = left num * the pre caclulated prefix of left num from prev iteration
+    // get prefix in res
+    let prefix = 1;
+    for(let i = 0; i < nums.length; i++) {
+        res[i] = prefix; // storing prefix in res
+        prefix *= nums[i]; // update prefix for next iteration num
     }
-    
-    const postfix = new Array(nums.length).fill(1);
-    // get postfix of all nums -> meaning product of all elements to the right of curr num
-    // by default the last element has a postfix of 1 since it has no elements to its right
-    for(let i = nums.length - 2; i >= 0; i--) {
-        postfix[i] = nums[i+1] * postfix[i+1]; // curr num postfix = right num * the pre caclulated postfix of right num from prev iteration
-    }
-  
-    const res = new Array(nums.length).fill(1);
-    // res is simply prefix * postfix
-    for(let i = 0; i < res.length; i++) {
-        res[i] = prefix[i] * postfix[i];
+   
+     // get postfix in res
+    let postfix = 1;
+    for(let i = nums.length - 1; i >= 0; i--) {
+        res[i] *= postfix; // storing prefix * postfix in res
+        postfix *= nums[i]; // update postfix for next iteration num
     }
     
     return res;
