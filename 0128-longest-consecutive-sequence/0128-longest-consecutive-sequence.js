6bd9be5f82656a6fd3c7ef5ca8f7528e654dfbf1
Lenny <lenny.j.remache@gmail.com>
2023-10-11 18:18:36 -0400

    Time: 106 ms (59.18%), Space: 64 MB (39.54%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -3,25 +3,21 @@
  * @return {number}
  */
 const longestConsecutive = (nums) => {
-    const map = {}; // O(n) space where n is length of nums
-    for(let num of nums) {
-        map[num] = num;
-    }
+    const numSet = new Set(nums); // O(n) space where n is length of nums
     
     let maxLength = 0;
-    for(let num in map) { 
-        let currNum = parseInt(num);
+    for(const num of numSet) { 
+
         // a starting number for a consecutive sequence has no left neighbor
-        if (map[currNum - 1] === undefined) {
+        if (!numSet.has(num-1)) {
             let length = 0;
-            while (map[currNum] !== undefined) {
+            let currNum = num;
+            while (numSet.has(currNum)) {
                 length += 1;
-                currNum = parseInt(currNum) + 1;
+                currNum += 1;
             }
             maxLength = Math.max(maxLength, length);
-        }
-       
-        
+        }        
     }
     
     return maxLength;
