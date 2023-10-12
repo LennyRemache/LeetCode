b5984b6fcacf28e24478564406817091b6fdec46
Lenny <lenny.j.remache@gmail.com>
2023-10-12 15:26:05 -0400

    Time: 49 ms (86.81%), Space: 42.1 MB (73.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,24 +2,36 @@
  * @param {string} s
  * @return {boolean}
  */
+
+// O(n) runtime since loop depends on length of s
+// O(n) space since stack depends on size of s 
+
 const isValid = (s) => {
+    // used to help track bracket valid pairs
     const brackets = {
         "{" : "}",
         "(" : ")",
         "[" : "]"
     }
     
-    const openStack = [];
+    // O(n) space worse case when all brackets are open brackets
+    const openStack = []; // track the order of each open bracket that appears. Once a close bracket appears we check if it matches with the most recent open bracket to appear
     
+    // O(n) runtime since we iterate through each bracket in s
+    // loop through each bracket in s
     for(let bracket of s) {
+        // if an open bracket we store the order of the open bracket by pushing it into the stack
         if (brackets[bracket] !== undefined) {
             openStack.push(bracket);
-        } else {
+        } else { // is a close bracket
+            // if the current close bracket does not match with the valid pair of the latest open bracket, return false
             if (bracket !== brackets[openStack.pop()]) {
                 return false;
             }
         }
     }
+    // loop exits once every close bracket finds its valid open bracket
     
+    // however theres a case where we have leftover open brackets from the start of s that did not find a close bracket
     return openStack.length === 0 ? true : false;
 };
