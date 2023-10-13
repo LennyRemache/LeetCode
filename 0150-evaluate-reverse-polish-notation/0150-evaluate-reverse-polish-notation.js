78e6d766622006d8dc0a88c49a55ce05db45cb94
Lenny <lenny.j.remache@gmail.com>
2023-10-13 18:20:45 -0400

    Time: 64 ms (46.10%), Space: 45.1 MB (54.44%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -3,27 +3,23 @@
  * @return {number}
  */
 const evalRPN = (tokens) => {
-    // T: O(n) loop through tokens once
-    // S: O(n) relative to the input  
+    const op = {
+        "+": (n1, n2) => n1 + n2,
+        "-": (n1, n2) => n1 - n2,
+        "*": (n1, n2) => n1 * n2,
+        "/": (n1, n2) => Math.trunc(n1 / n2)
+    };
+    const numStack = [];
     
-    // objects can have have keys mapped to a function
-    const validOps = {
-        "+": (num1, num2) => num1 + num2,
-        "-": (num1, num2) => num1 - num2,
-        "*": (num1, num2) => num1 * num2,
-        "/": (num1, num2) => Math.trunc(num1 / num2) // note from description says division should always truncate to 0
-    }
-    
-    let numStack = [];
-    
-    for(let token of tokens) {
-        // if token is a number, push it into the numStack
-        if (validOps[token] === undefined) {
-            numStack.push(parseInt(token));
-        } else { // else token is an arithmetic operator
+    for (const token of tokens) {
+        if (op[token] !== undefined) {
             const num2 = numStack.pop();
             const num1 = numStack.pop();
-            numStack.push(validOps[token](num1, num2));
+            const newNum = op[token](num1, num2);
+            numStack.push(newNum);
+           
+        } else {
+            numStack.push(parseInt(token));
         }
     }
     
