bd88764800c270618d5703c45d45f17e96eb867c
Lenny <lenny.j.remache@gmail.com>
2023-10-13 20:09:42 -0400

    Time: 47 ms (86.17%), Space: 42.7 MB (56.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,29 +3,34 @@
  * @return {string[]}
  */
 const generateParenthesis = (n) => {
-    let open = 0;
-    let close = 0;
     
     let combination = [];
     let res = [];
+    
     const backtrack = (open, close) => {
+        // Base Case
+        // when theres already n open and n close parenthesis then we have a valid combination
         if (open === n && close === n) {
             res.push(combination.join(""));
             return;
         } 
         
+        // this is where the backtracking actually happens
+        // first add an open parenthesis if possible
+        // then add close parenthesis if possible
+        
         // add open parenthesis
         if (open < n) {
             combination.push("(");
             backtrack(open + 1, close);
-            combination.pop();
+            combination.pop(); // pop to allow the case where we instead add a close parenthesis
             
         }
         // add close parenthesis
         if (close < open) {
             combination.push(")");
             backtrack(open, close + 1);
-            combination.pop();
+            combination.pop(); // pop to allow the case where we instead add an open parenthesis
         }
     }
     backtrack(0,0);
