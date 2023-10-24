c104b3df5ff0d5da4018546c015044623e052e26
Lenny <lenny.j.remache@gmail.com>
2023-10-24 18:03:57 -0400

    Time: 221 ms (61.49%), Space: 80.7 MB (10.61%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -2,6 +2,10 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
+
+// O(n) runtime since at most each temp will be seen twice 
+// O(n) space for the resulting array and monotonic stack
+
 const dailyTemperatures = (temperatures) => {
     const res = new Array(temperatures.length).fill(0);
     
@@ -11,11 +15,15 @@ const dailyTemperatures = (temperatures) => {
     for(let i = 0; i < temperatures.length; i++) {
         const currTemp = temperatures[i];
         
+        // only way we can compare prev temps with curr temp is to have a stack with values
+        // then we can check if curr temp is greater than the prev temp at the top of the stack
         while (stack.length > 0 && currTemp > stack[stack.length-1][0]) {
+            // if yes then we can continously pop the top of the stack until prev temp is not less than curr temp
             const [temp, index] = stack.pop();
-            res[index] = i - index;
+            res[index] = i - index; // prev temp's # of days to get a greater temp is calculated by prev temps index and curr temps index
+            // it would be stored at the same index in res as in temperatures
         }
-        stack.push([currTemp, i])
+        stack.push([currTemp, i]) // setting up for next iteration to find # of days to find a temp greater than the curr temp
     }
     
     return res;
