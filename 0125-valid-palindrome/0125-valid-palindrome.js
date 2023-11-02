e0872b073d49f4d4f42675ca7381b4a20fd91f19
Lenny <lenny.j.remache@gmail.com>
2023-11-01 20:08:38 -0400

    Time: 61 ms (67.37%), Space: 45.2 MB (63.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,12 +3,16 @@
  * @return {boolean}
  */
 
+// O(n) runtime since we iterate through each character once
+// O(1) space since we do not use additional space. We simply use the same input string to iterate over
+
 const isPalindrome = (s) => {
-    s = s.toLowerCase()
+    s = s.toLowerCase() // description says to convert all uppercase chars to lowercase before checking if palindrome
     
-    let left = 0;
-    let right = s.length - 1;
+    let left = 0; // start pointer to check left side
+    let right = s.length - 1; // end pointer to check right side
     
+    // while left pointer is less than right pointer because after left become right we would be checking the same chars again
     while (left < right) {
         const isLeftNum = 48 <= s.charCodeAt(left) && s.charCodeAt(left) <= 57;
         const isRightNum = 48 <= s.charCodeAt(right) && s.charCodeAt(right) <= 57;
@@ -16,17 +20,18 @@ const isPalindrome = (s) => {
         const isLeftAlpha = 97 <= s.charCodeAt(left) && s.charCodeAt(left) <= 122;
         const isRightAlpha = 97 <= s.charCodeAt(right) && s.charCodeAt(right) <= 122;
         
+        // check if left is not a valid char
         if (!isLeftNum && !isLeftAlpha) {
             left += 1;
-        } else if (!isRightNum && !isRightAlpha) {
+        } else if (!isRightNum && !isRightAlpha) { // check if right is also not a valid char
             right -= 1;
-        } else if (s[left] === s[right]) {
+        } else if (s[left] === s[right]) { // otherwise left and right are valid chars so check if they are the same char
             left += 1;
             right -= 1;
         } else {
-            return false;
+            return false; // if not same chars then it is not a valid palindrome
         }
     }
     
-    return true;
+    return true; // is a valid palindrome if loop exits
 };
