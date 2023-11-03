59ae879e844e0f1d7c6db1ae14f45e677aa9263b
Lenny <lenny.j.remache@gmail.com>
2023-11-03 19:10:46 -0400

    Time: 143 ms (84.35%), Space: 58.9 MB (64.91%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -9,13 +9,12 @@ const threeSum = (nums) => {
     
     for(let i = 0; i < nums.length; i++) {
         if (nums[i] === nums[i-1]) continue; // prevent repeat triplets for the same number
-        
+        const subTarget = nums[i] * (-1);
         let left = i+1;
         let right = nums.length - 1;
-        
         while(left < right) {
-            const sum = nums[i] + nums[left] + nums[right];
-            if (sum === 0) {
+            const sum = nums[left] + nums[right];
+            if (sum === subTarget) {
                 res.push([nums[i], nums[left], nums[right]]);
                 left += 1;
                 right -= 1;
@@ -25,9 +24,9 @@ const threeSum = (nums) => {
                 while (left < right && nums[right] === nums[right+1]) {
                     right -= 1;
                 }
-            } else if (sum < 0) {
+            } else if (sum < subTarget) {
                 left += 1;
-            } else if (sum > 0) {
+            } else {
                 right -= 1;
             }
         }
